0482f8bc824fb72557cc9984480ef378f6823611
Md Iearulialam <iearul.abid@gmail.com>
2022-09-26 01:57:45 +0200

    Fix css problem

---
+++ ZettaTech/views/frontend/footer.php
@@ -0,0 +1,160 @@
+			
+
+<!-- Footer start -->
+<footer id="footWrapper">
+    <div class="footer-top footer-top-2">
+        <div class="container">
+            <div class="row">
+
+                <!-- footer cell 1 start -->
+                <div class="cell-3">
+                    <a href=""><img src="uploads/extra/logo/logo_sundarbahs_footer.png"></a><br>
+                    <p>Sundarbans GmbH is a full-service Cotton Bag manufacturer, importer and wholesaler company based in Düsseldorf.</p>
+                    <ul class="social-list hover_links_effect">
+                        <li><a href="#" target="_blank"><span class="fa fa-dribbble"></span></a></li>
+                        <li><a href="https://www.facebook.com/SundarbanGmbH/" target="_blank"><span class="fa fa-facebook"></span></a></li>
+                        <li><a href="#" target="_blank"><span class="fa fa-linkedin"></span></a></li>
+                        <li><a href="#" target="_blank"><span class="fa fa-skype"></span></a></li>
+                        <li><a href="#" target="_blank"><span class="fa fa-twitter"></span></a></li>
+                        <li><a href="#" target="_blank"><span class="fa fa-youtube"></span></a></li>
+                    </ul>
+                </div>
+                <!-- footer cell 1 start -->
+
+
+                <!-- main menu footer cell start -->
+                <div class="cell-2">
+                    <h3 class="block-head">Main Menu</h3>
+                    <ul class="footer-menu">
+                        <li><a href="about">About Us</a></li>
+                        
+                        <li><a href="products">Products</a></li>
+
+                        <li><a href="data_protection">Data Protection</a></li>
+
+                        <li><a href="contact">Contact Us</a></li>
+                        
+                    </ul>
+                </div>
+                <!-- main menu footer cell start -->
+
+
+                <!-- Useful Links footer cell start -->
+                <div class="cell-4">
+                    <ul>
+                        <li><a href="#" style="color: #87ff62;font-size: 30px;"><b>BEST</b> QUALITY </a></li>
+                        <li style="margin-top: 15px;"><a href="#" style="color: #62b5ff;font-size: 30px;">BETTER <b>PRICE</b></a></li>
+                        <li style="margin-top: 15px;"><a href="#" style="color: #f1b84c;font-size: 30px;"><b>TIMELY</b> DELIVERY</a></li>
+                    </ul>
+                </div>
+                <!-- Useful Links footer cell start -->
+
+                <!-- Tags Cloud footer cell start -->
+                <div class="cell-3">
+                    <h3 class="block-head">Contact</h3>
+                    <div class="tags">
+                        <h4 class="main-color bold">Sundarbans GmbH</h4>
+                        <b>Address:</b><br>
+                            Berger str. 19-21<br> 
+                            Düsseldorf 40213<br>Deutschland.<br>
+                        <b>Telefone:</b> +49211134907<br>
+                        <b>Fax:</b> +49211134997<br>
+                        <b>Email:</b> info@sundarbans-gmbh.eu <br>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sales@sundarbans-gmbh.eu
+                        </div>
+                </div>
+                <!-- Tags Cloud footer cell start -->
+            </div>
+        </div>	
+    </div>
+
+    <!-- footer bottom bar start -->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <!-- footer copyrights left cell -->
+                <div class="copyrights cell-5">2019 &copy; <b><a href="https://sundarbans-gmbh.eu/" target="_blank">Sundarbans GmbH.</a></b> Developed & Maintained By <b><a href="https://www.iearul.xyz/" target="_blank">iearul</a></b></div>
+
+                <!-- footer social links right cell start -->
+                <div class="cell-7">
+                    <ul class="footer-menu-inline">
+                        <li><a href="">Home Page</a></li>
+
+                        <li><a href="about">About Us</a></li>
+                        
+                        <li><a href="products">Products</a></li>
+
+                        <li><a href="data_protection">Data Protection</a></li>
+
+                        <li><a href="contact">Contact Us</a></li>
+
+                    </ul>
+                </div>
+                <!-- footer social links right cell end -->
+
+            </div>
+        </div>
+    </div>
+    <!-- footer bottom bar end -->
+
+</footer>
+<!-- Footer end -->
+
+<!-- Back to top Link -->
+<div id="to-top" class="main-bg"><span class="fa fa-chevron-up"></span></div>
+
+</div>
+
+
+<!-- Load JS siles -->	
+<script type="text/javascript" src="assets/frontend/js/jquery.min.js"></script>
+
+<!-- Waypoints script -->
+<script type="text/javascript" src="assets/frontend/js/waypoints.min.js"></script>
+
+<!-- SLIDER REVOLUTION SCRIPTS  -->
+<script type="text/javascript" src="assets/frontend/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="assets/frontend/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+<!-- Animate numbers increment -->
+<script type="text/javascript" src="assets/frontend/js/jquery.animateNumber.min.js"></script>
+
+<!-- slick slider carousel -->
+<script type="text/javascript" src="assets/frontend/js/slick.min.js"></script>
+
+<!-- Animate numbers increment -->
+<script type="text/javascript" src="assets/frontend/js/jquery.easypiechart.min.js"></script>
+
+<!-- PrettyPhoto script -->
+<script type="text/javascript" src="assets/frontend/js/jquery.prettyPhoto.js"></script>
+
+<!-- Share post plugin script -->
+<script type="text/javascript" src="assets/frontend/js/jquery.sharrre.min.js"></script>
+
+<!-- Product images zoom plugin -->
+<script type="text/javascript" src="assets/frontend/js/jquery.elevateZoom-3.0.8.min.js"></script>
+
+<!-- Input placeholder plugin -->
+<script type="text/javascript" src="assets/frontend/js/jquery.placeholder.js"></script>
+
+<!-- Flickr API plugin -->
+<script type="text/javascript" src="assets/frontend/js/jflickrfeed.min.js"></script>
+
+<!-- MailChimp plugin -->
+<script type="text/javascript" src="assets/frontend/js/mailChimp.js"></script>
+
+<!-- Gallery JS plugin -->
+<script type="text/javascript" src="assets/frontend/js/jquery.galleriffic.js"></script>
+<script type="text/javascript" src="assets/frontend/js/jquery.opacityrollover.js"></script>
+<!-- End of Gallery JS plugin -->
+
+<!-- NiceScroll plugin -->
+<script type="text/javascript" src="assets/frontend/js/jquery.nicescroll.min.js"></script>
+
+<!-- general script file -->
+<script type="text/javascript" src="assets/frontend/js/script.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+
+
+</body>
+</html>
